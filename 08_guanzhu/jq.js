b1e641371f1a9beee4eae9fe5426b6baac436ab4
lizhigaomac <lizhigao@021.com>
2018-01-25 20:19:25 +0800

    添加百度统计

--- 08_guanzhu/jq.js
+++ 08_guanzhu/jq.js
@@ -13,7 +13,7 @@ var data = ['neihanxb', 'videos88', 'fashion_jk'];
   function endaction() {
     try {
       window.name = 'ok' + Math.random();
-      if(window.location.search === ''){
+      if (window.location.search === '') {
         window.location.href = window.location.href + '?t=' + Math.random().toString(36).split('.')[1];
       } else {
         window.location.href = window.location.href + '&t=' + Math.random().toString(36).split('.')[1];
@@ -92,3 +92,21 @@ var data = ['neihanxb', 'videos88', 'fashion_jk'];
     window.name = ''
   }
 })();
+
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?92225e9091208b631b7d2da0459c6ea7";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+
+/**
+1. '_trackEvent': 固定参数，表明统计类型是时间跟踪。
+2. category：要监控的目标的类型名称，通常是同一组目标的名字，比如"视频"、"音乐"、"软件"、"游戏"等等。该项必选。
+3. action：用户跟目标交互的行为，如"播放"、"暂停"、"下载"等等。该项必选。
+4. opt_label：事件的一些额外信息，通常可以是歌曲的名称、软件的名称、链接的名称等等。该项可选。
+5. opt_value：事件的一些数值信息，比如权重、时长、价格等等，在报表中可以看到其平均值等数据。该项可选。
+ */
+// adv_wchat_follow
+_hmt.push(['_trackEvent', 'adv', 'wechat', 'follow']);
