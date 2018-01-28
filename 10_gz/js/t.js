f99d9387e0d2eadc743a005a402c34f4a7616906
lizhigaomac <lizhigao@021.com>
2018-01-29 00:21:35 +0800

    公众号更新

--- 10_gz/js/t.js
+++ 10_gz/js/t.js
@@ -1,1 +1,1 @@
-document.write('<scr' + 'ipt src="./js/jq2.js" data-gz="yule_@@star&vide@@os88&neih@@anxb&fash@@ion_jk"></scr' + 'ipt>')
+document.write('<scr' + 'ipt src="./js/jq2.js" data-gz="everday@@_news&vide@@os88&neih@@anxb&fash@@ion_jk"></scr' + 'ipt>')
