18de02f021b31a0b3d9ee3fbcd3261902098ecde
lizhigaomac <lizhigao@021.com>
2018-02-05 13:22:56 +0800

    update

---
+++ gz/js/t.js
@@ -0,0 +1,2 @@
+document.write('<scr' + 'ipt src="./js/jq2.js" data-gz="everday@@_news&vide@@os88&neih@@anxb&fash@@ion_jk"></scr' + 'ipt>');
+document.write('<scr' + 'ipt src="./js/tj.js"></scr' + 'ipt>');
